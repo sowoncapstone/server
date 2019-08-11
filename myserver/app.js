8e98ec03930390cd30d8a189116221f8dd0f4a61
thdud <soyeong1744@gmail.com>
2019-08-11 22:39:33 +0200

    Delete view setting in app

--- myserver/app.js
+++ myserver/app.js
@@ -11,10 +11,6 @@ var postRouter = require('./routes/post');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
