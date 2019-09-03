5bd90e3d07d0bf49460798cebf4b4fd8daa86af1
thdud <soyeong1744@gmail.com>
2019-09-03 19:18:41 +0900

    Change limit

--- myserver/app.js
+++ myserver/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var loginRouter = require('./routes/login');
@@ -17,7 +18,8 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
